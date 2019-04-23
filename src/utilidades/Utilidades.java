147a4ed8df0a7bc95983f72284254ddba19366e2
Ángela <34713438+randomkwiz@users.noreply.github.com>
2019-04-23 20:06:39 +0200

    Método de ordenación

--- src/utilidades/Utilidades.java
+++ src/utilidades/Utilidades.java
@@ -1,6 +1,7 @@
 package utilidades;
 
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class Utilidades {
     /*
@@ -29,4 +30,68 @@ public class Utilidades {
     	System.out.println("Numero de cuenta: " + campos[0]);
     	System.out.println("Saldo: " + campos[1]);
     }
+
+    /*
+    * INTERFAZ
+    * Signatura: public void ordenarFicheroPorClave(String ruta, int campoClave)
+    * Comentario: ordena el fichero de una ruta en base a la lexicografía de un campo de un fichero
+    * Precondiciones:
+    * Entradas: Se pasará como String la ruta del fichero a ordenar y como entero el campo clave que se evaluará para ordenar
+    * Salidas:
+    * Postcondiciones: El fichero quedará ordenado en base a ese campo lexicográficamente.
+    * */
+    public void ordenarFicheroPorClave(String ruta, int campoClave){
+        File ficheroAOrdenar = new File(ruta);
+        FileReader fr = null;
+        FileWriter fw = null;
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+        List<String> registros = new ArrayList<String>();    //arraylist - considerar cambiar a array
+        String registro = " ";
+        String aux=" "; //para el bubblesort de mÃ¡s abajo
+
+        try{
+            fr = new FileReader(ficheroAOrdenar);
+            br = new BufferedReader(fr);
+            while(br.ready()){
+                registro = br.readLine();
+                registros.add(registro);
+            }
+            br.close();
+            fr.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        //ordenamiento burbuja
+
+        for (int i = 0; i < registros.size()-1;i++){
+
+            for (int j = registros.size()-1; j>i; j--){
+                if (registros.get(j).compareTo(registros.get(j-1)) < 0) {
+                    //se produce el intercambio de elementos
+                    aux = registros.get(j);
+                    registros.set(j,registros.get(j-1));
+                    registros.set(j-1, aux);
+                }
+            }
+        }
+        try{
+            fw = new FileWriter(ficheroAOrdenar);
+            bw = new BufferedWriter(fw);
+
+            for(int i = 0; i < registros.size(); i ++){
+                bw.write(registros.get(i));
+                bw.newLine();
+            }
+            bw.close();
+            fw.close();
+        }catch ( IOException e ){
+            e.printStackTrace();
+        }
+
+
+
+    }
+
 }
