0a44824e9d27ee705ea94da30ae4c8a7bc3bad57
Ángela <34713438+randomkwiz@users.noreply.github.com>
2019-04-24 19:17:15 +0200

    Probados los ficheros binarios. Salta una excepción

--- src/tests/UtilidadesTest.java
+++ src/tests/UtilidadesTest.java
@@ -1,9 +1,15 @@
 package tests;
+import gestion.GestionBancoCentral;
 import utilidades.Utilidades;
+
+import java.util.GregorianCalendar;
+
 public class UtilidadesTest {
     public static void main(String[] args) {
         Utilidades util = new Utilidades();
-        util.ordenarFicheroPorClave("./prueba.txt",1);
-        //System.out.println("Hola");
+        GestionBancoCentral g = new GestionBancoCentral();
+        g.insertarMovimientoEnFicheroMovimientos("ESPCAIXESBBXXX0000000",true,"Hola",45,new GregorianCalendar());
+
+        util.imprimirMovimientos(g.ultimosDiezMovimientos("ESPCAIXESBBXXX0000000"));
     }
 }
